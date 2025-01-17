ca6d5af1dfbcd875da484ebcb97d9ed752528c37
Tanya Maxar <kolt.levry@gmail.com>
2025-01-17 09:53:05 +0100

    start work

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 
 import "./App.css";
+import Xapp from './components/x_try_react/Xapp'; // Імпортуємо компонент
+import { logMessage } from "./components/loghelper/Loghelper";
 
 function App() {
-  return <div className="app">Hello, React</div>;
+  logMessage(); // Викликаємо утилітну функцію
+  return (
+  <div className="app">
+    <h1>Hello, React</h1>
+    <Xapp />  {/* Використовуємо компонент */}
+  </div>
+  );
 }
 
 export default App;
---
+++ src/components/x_try_react/Xapp.jsx
@@ -0,0 +1,10 @@
+function Xapp() {
+    console.log("Hi");
+    return (
+        <div>
+            <p>This is Xapp component</p>
+        </div>
+    );
+}
+
+export default Xapp;
